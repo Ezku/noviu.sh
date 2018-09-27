3f807bbe2f8cf644add166288d9ee54ca733a731
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-09-27 15:45:05 +0300

    Throw in Meyer's css reset

--- pages/_app.js
+++ pages/_app.js
@@ -9,13 +9,62 @@ if (typeof window !== 'undefined') {
 }
 
 injectGlobal`
+/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+`;
+
+injectGlobal`
   @import url("https://use.typekit.net/lgx6cgr.css");
-  * {
-    font-family: neue-haas-grotesk-text, sans-serif;
-  }
-  html {
+  body {
     font-size: 16px;
     font-weight: 400;
+    font-family: neue-haas-grotesk-text, sans-serif;
   }
   h1, h2, h3, h4, h5, h6 {
     font-family: neue-haas-grotesk-display, sans-serif;
