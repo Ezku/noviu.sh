0561dfb28e5d123295ffe7e0aa2442c73d2c2112
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-09-27 18:43:09 +0300

    Fix source ordering for global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,75 +1,12 @@
 import App, { Container } from 'next/app';
 import React from 'react';
-import { hydrate, injectGlobal } from 'react-emotion';
+import { hydrate } from 'react-emotion';
 
 // Adds server generated styles to emotion cache.
 // '__NEXT_DATA__.ids' is set in '_document.js'
 if (typeof window !== 'undefined') {
   hydrate(window.__NEXT_DATA__.ids);
 }
-
-injectGlobal`
-/* http://meyerweb.com/eric/tools/css/reset/ 
-   v2.0 | 20110126
-   License: none (public domain)
-*/
-
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-	display: block;
-}
-body {
-	line-height: 1;
-}
-ol, ul {
-	list-style: none;
-}
-blockquote, q {
-	quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-	content: '';
-	content: none;
-}
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
-}
-`;
-
-injectGlobal`
-  @import url("https://use.typekit.net/lgx6cgr.css");
-  body {
-    font-size: 16px;
-    font-weight: 400;
-    font-family: neue-haas-grotesk-text, sans-serif;
-  }
-  h1, h2, h3, h4, h5, h6 {
-    font-family: neue-haas-grotesk-text, sans-serif;
-  }
-`;
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
